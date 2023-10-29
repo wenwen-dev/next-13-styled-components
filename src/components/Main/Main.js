94ff8871601bdf8b2fc63ae266a5c73f7f866be8
Wenwen Wang <wenwen.wangwen@gmail.com>
2023-10-29 08:23:48 -0700

    Complete my solution

---
+++ src/components/Main/Main.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const MainWrapper = styled.main`
+  width: 100%;
+  max-width: 800px;
+  padding: 16px 24px;
+  margin: 0px auto;
+  border: 1px solid hsl(0deg 0% 50% / 0.3);
+  border-radius: 2px;
+  background: white;
+`;
+
+function Main({ children }) {
+  return <MainWrapper>{children}</MainWrapper>;
+}
+
+export default Main;
